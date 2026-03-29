09b34746d87220d9724c6771af6e904079cb12f1
Luis Armando Oriens <luisarmando.murillobaltodano@gmail.com>
2026-03-29 11:02:57 -0600

    Add

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || "",
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL || "",
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY || "",
+  },
   typescript: {
     ignoreBuildErrors: false,
   },
